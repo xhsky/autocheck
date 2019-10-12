785331ad9879694b3b9cb2165f09166ca70a8514
sky <x_hsky@163.com>
2019-10-12 14:26:11 +0800

    自动建立report目录

--- lib/printf.py
+++ lib/printf.py
@@ -2,11 +2,12 @@
 # *-* coding:utf8 *-*
 # sky
 
-def printf(message):
-    with open("./check.info", "a", encoding="utf8") as f:
-        f.write(f"{message}\n")
+import os
 
-    
+def printf(message):
+    os.makedirs("report", exist_ok=True)
+    with open("./report/check.info", "a", encoding="utf8") as f:
+        f.write(f"{message}\n")
 
     
 if __name__ == "__main__":
