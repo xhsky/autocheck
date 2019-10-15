97e9023b77bd75fa5014fba2bd19d46afddcfb46
sky <x_hsky@163.com>
2019-10-15 10:40:32 +0800

    判断是否安装了cron服务

--- core/timing.py
+++ core/timing.py
@@ -21,13 +21,17 @@ def timing():
             exec_file=f"{os.path.dirname(os.path.dirname(__file__))}/main.py"
             timing_msg=f"{minute} {hour} {day} {month} {week} {python_bin} {exec_file}"
             crontabe_file="/var/spool/cron/root"
-            with open(crontabe_file, "r") as f:
-                for line in f.readlines():
-                    if line.strip()==timing_msg:
-                        break
-                else:
-                    with open(crontabe_file, "a") as f1:
-                        f1.write(f"{timing_msg}\n")
+            if os.path.exists(crontabe_file):
+                with open(crontabe_file, "r") as f:
+                    for line in f.readlines():
+                        if line.strip()==timing_msg:
+                            break
+                    else:
+                        with open(crontabe_file, "a") as f1:
+                            f1.write(f"{timing_msg}\n")
+
+            else:
+                print("未找到{crontab_file}文件, 请按照cron服务")
         else:
             print("配置文件中{pthon_bin}不存在")
 
