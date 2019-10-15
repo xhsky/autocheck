1418c9b05434f9c1002405b7babe2918b9c81611
sky <x_hsky@163.com>
2019-10-15 10:49:04 +0800

    修改

--- core/timing.py
+++ core/timing.py
@@ -20,20 +20,20 @@ def timing():
         if os.path.exists(python_bin):
             exec_file=f"{os.path.dirname(os.path.dirname(__file__))}/main.py"
             timing_msg=f"{minute} {hour} {day} {month} {week} {python_bin} {exec_file}"
-            crontabe_file="/var/spool/cron/root"
-            if os.path.exists(crontabe_file):
-                with open(crontabe_file, "r") as f:
+            crontab_file="/var/spool/cron/root"
+            if os.path.exists(crontab_file):
+                with open(crontab_file, "r") as f:
                     for line in f.readlines():
                         if line.strip()==timing_msg:
                             break
                     else:
-                        with open(crontabe_file, "a") as f1:
+                        with open(crontab_file, "a") as f1:
                             f1.write(f"{timing_msg}\n")
 
             else:
-                print("未找到{crontab_file}文件, 请按照cron服务")
+                print(f"未找到{crontab_file}文件, 请安装cron服务")
         else:
-            print("配置文件中{pthon_bin}不存在")
+            print(f"配置文件中{pthon_bin}不存在")
 
     
 if __name__ == "__main__":
