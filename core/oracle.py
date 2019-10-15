763b48e21a40820ec9e57ad4a40cf3655205df6f
sky <x_hsky@163.com>
2019-10-15 10:16:04 +0800

    判断Oracle是否连接

--- core/oracle.py
+++ core/oracle.py
@@ -39,37 +39,40 @@ def info():
         """
         cmd=f"su - oracle -c 'sqlplus -S / as sysdba <<EOF\n{sql}\nEOF'"
         (status, message)=subprocess.getstatusoutput(cmd)
-        printf(f"表空间名称    表空间大小(M)    已使用表空间(M)    未使用表空间(M)    使用率(%)")
-        for i in message.splitlines():
-            i=i.split()
-            row=f"{i[0]:<14}{float(i[1]):<17.02f}{float(i[2]):<20.02f}{float(i[3]):<18.02f}{float(i[4]):<8.02f}"
-            printf(row)
-
-        # awr
-        printf("-"*40)
-        printf("awr信息:")
-        awr_format="html"
-        awr_file='/tmp/awr.html'
-        days=4
-        sql="""
-        set heading off trimspool on feedback off pagesize 0
-        select trim(max(snap_id)) from dba_hist_snapshot;
-        """
-        cmd=f"su - oracle -c 'sqlplus -S / as sysdba <<EOF\n{sql}\nEOF'"
-        (status, message)=subprocess.getstatusoutput(cmd)
-        max_snap_id=int(message)
-        if int(awr_hours) > max_snap_id:
-            awr_hours=max_snap_id
-        min_snap_id=max_snap_id-int(awr_hours)
-        cmd=f"""su - oracle -c"
-        echo -e '{awr_format}\n{days}\n{min_snap_id}\n{max_snap_id}\n{awr_file}\n' | (sqlplus -S / as sysdba @?/rdbms/admin/awrrpt.sql)"
-        """
-        (status, message)=subprocess.getstatusoutput(cmd)
         if status==0:
-            shutil.move(awr_file, "./report/")
-            printf("请查看awr.html文件")
+            printf(f"表空间名称    表空间大小(M)    已使用表空间(M)    未使用表空间(M)    使用率(%)")
+            for i in message.splitlines():
+                i=i.split()
+                row=f"{i[0]:<14}{float(i[1]):<17.02f}{float(i[2]):<20.02f}{float(i[3]):<18.02f}{float(i[4]):<8.02f}"
+                printf(row)
+
+            # awr
+            printf("-"*40)
+            printf("awr信息:")
+            awr_format="html"
+            awr_file='/tmp/awr.html'
+            days=4
+            sql="""
+            set heading off trimspool on feedback off pagesize 0
+            select trim(max(snap_id)) from dba_hist_snapshot;
+            """
+            cmd=f"su - oracle -c 'sqlplus -S / as sysdba <<EOF\n{sql}\nEOF'"
+            (status, message)=subprocess.getstatusoutput(cmd)
+            max_snap_id=int(message)
+            if int(awr_hours) > max_snap_id:
+                awr_hours=max_snap_id
+            min_snap_id=max_snap_id-int(awr_hours)
+            cmd=f"""su - oracle -c"
+            echo -e '{awr_format}\n{days}\n{min_snap_id}\n{max_snap_id}\n{awr_file}\n' | (sqlplus -S / as sysdba @?/rdbms/admin/awrrpt.sql)"
+            """
+            (status, message)=subprocess.getstatusoutput(cmd)
+            if status==0:
+                shutil.move(awr_file, "./report/")
+                printf("请查看awr.html文件")
+            else:
+                printf("awr生成失败, 请手动生成")
         else:
-            printf("awr生成失败, 请手动生成")
+            printf("无法连接Oracle.")
 
 if __name__ == "__main__":
     main()
