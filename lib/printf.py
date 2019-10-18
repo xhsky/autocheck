5e3db53d60a07d43c3677367e669fdfc562dc602
sky <x_hsky@163.com>
2019-10-18 16:26:03 +0800

    添加写入预警文件warning

--- lib/printf.py
+++ lib/printf.py
@@ -2,9 +2,14 @@
 # *-* coding:utf8 *-*
 # sky
 
-def printf(message):
-    with open("./report/check.info", "a", encoding="utf8") as f:
-        f.write(f"{message}\n")
+def printf(message, mail_body=0):
+    if mail_body==0:
+        with open("./report/check.info", "a", encoding="utf8") as f:
+            f.write(f"{message}\n")
+    elif mail_body==1:
+        with open("./report/warning", "a", encoding="utf8") as f:
+            f.write(f"{message}\n")
+
 
     
 if __name__ == "__main__":
