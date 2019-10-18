39140cc0708cd5463b096231dc4ba3e15a09f1d3
sky <x_hsky@163.com>
2019-10-18 17:23:44 +0800

    将预警信息直接显示在邮件正文

--- core/mail.py
+++ core/mail.py
@@ -2,12 +2,6 @@
 # *-* coding:utf8 *-*
 # sky
 
-"""
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from email.header import Header
-"""
 import smtplib  # 加载smtplib模块
 from email.mime.text import MIMEText
 from email.utils import formataddr
@@ -17,6 +11,7 @@ from email.mime.multipart import MIMEMultipart
 
 from lib import conf
 import datetime, os
+import subprocess
 
 def send():
     check, sender_alias, receive, subject=conf.get("mail", 
@@ -36,18 +31,32 @@ def send():
 
             sender="check@dreamdt.cn"
             password="DreamSoft#1dream"
-
             smtp_server="smtp.dreamdt.cn"
             smtp_port=25
+
             message=MIMEMultipart('related')
             message['From']=formataddr([sender_alias, sender])
             receive_list=[]
             for i in receive.split(","):
                 receive_list.append(i.strip())
             message['To']=','.join(receive_list)
+
             subject=f"{subject}-{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
             message['Subject']=subject
-            #message.attach(MIMEText("测试", "plain", "utf-8"))
+
+            warning_file="./report/warning"
+            if os.path.exists(warning_file):
+                with open(warning_file, "r") as f:
+                    mail_body=f.read()
+            else:
+                mail_body="今天无预警内容"
+
+            mail_body=f"预警信息:\n{mail_body}\n\n详细巡检信息请查看附件."
+            cmd='top -b -n 1 -o %MEM | head -n 15'
+            status, top_msg=subprocess.getstatusoutput(cmd)
+            mail_body=f"主机负载简图:\n{top_msg}\n\n\n{'*'*80}\n\n\n{mail_body}"
+
+            message.attach(MIMEText(mail_body, "plain", "utf-8"))
 
             all_file=os.listdir(".")    # 获取附件文件名称
             for i in all_file:
