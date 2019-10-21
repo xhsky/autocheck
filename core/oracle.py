521eea608fc404beb29d0d26e3133ce1d4ef8b4b
sky <x_hsky@163.com>
2019-10-21 15:29:57 +0800

    oracle表空间

--- core/oracle.py
+++ core/oracle.py
@@ -4,13 +4,13 @@
 
 import subprocess
 from lib import conf
+from lib.tools import format_size
 from lib.printf import printf
 import shutil
 
 def info():
-    check, oracle_sid, awr_hours=conf.get("oracle", 
+    check, awr_hours=conf.get("oracle", 
             "check", 
-            "oracle_sid", 
             "awr_hours"
             )
     
@@ -21,9 +21,9 @@ def info():
         sql="""
         set heading off trimspool on feedback off pagesize 0 linesize 1000
         SELECT a.tablespace_name ,
-          a.bytes/ 1024 / 1024,
-          ( a.bytes - b.bytes ) / 1024 / 1024 ,
-          b.bytes / 1024 / 1024 ,
+          a.bytes,
+          ( a.bytes - b.bytes ),
+          b.bytes,
           Round(( ( a.bytes - b.bytes ) / a.bytes ) * 100, 2)
         FROM  (SELECT tablespace_name,
                       SUM(bytes) bytes
@@ -40,10 +40,10 @@ def info():
         cmd=f"su - oracle -c 'sqlplus -S / as sysdba <<EOF\n{sql}\nEOF'"
         (status, message)=subprocess.getstatusoutput(cmd)
         if status==0:
-            printf(f"表空间名称    表空间大小(M)    已使用表空间(M)    未使用表空间(M)    使用率(%)")
+            printf(f"表空间名称    表空间大小    已使用表空间    未使用表空间    使用率(%)")
             for i in message.splitlines():
                 i=i.split()
-                row=f"{i[0]:<14}{float(i[1]):<17.02f}{float(i[2]):<20.02f}{float(i[3]):<18.02f}{float(i[4]):<8.02f}"
+                row=f"{i[0]:<14}{format_size(i[1]):<17}{format_size(i[2]):<20}{format_size(i[3]):<18}{format_size(i[4]):<8}"
                 printf(row)
 
             # awr
