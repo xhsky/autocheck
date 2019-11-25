be8bacc580d0532a205c9227cc090fc9b72c87fa
sky <x_hsky@163.com>
2019-11-25 15:09:16 +0800

    改为后台进程并加入启动控制

--- main.py
+++ main.py
@@ -2,26 +2,13 @@
 # *-* coding:utf8 *-*
 # sky
 
-#from core import host, tomcat, redis, mysql, mail, timing, oracle, backup
-#from lib import conf
-#import os, datetime, tarfile, sys
-#import socket, shutil
-
-from core import record as rec, analysis as ana, show as sh
+from core import record, analysis, show
 from lib import database, log
-import os, sys
+import os, sys, atexit, time
 import gevent                                                                                                                                                                 
 from gevent import monkey
 import configparser
-
-def record():
-    rec.record()
-
-def show():
-    sh.show()
-
-def analysis():
-    ana.analysis()
+import psutil
 
 def get_config(cfg, section, option):
     """读取配置文件, 返回value
@@ -35,7 +22,21 @@ def get_config(cfg, section, option):
     else:
         return None
 
-def config_to_db():
+def init(data_file, init_file):
+    """数据初始化
+    """
+    os.makedirs(os.path.dirname(data_file), exist_ok=True)
+    if os.path.exists(data_file):
+        pass
+    else:
+        logger.logger.info("开始初始化数据.")
+        db=database.db(data_file)
+        with open(init_file, "r") as f:
+            for sql in f.readlines():
+                db.update_one(sql)
+        logger.logger.info("初始化数据完成.")
+
+def config_to_db(config_file):
     """读取配置文件, 将配置写入db
     """
     logger.logger.debug("开始读取配置文件...")
@@ -44,7 +45,7 @@ def config_to_db():
     config_init=db.query_all(sql)
 
     cfg=configparser.ConfigParser()
-    cfg.read("./conf/autocheck.conf")
+    cfg.read(config_file)
 
     config_list=[]
     for i in config_init:
@@ -58,124 +59,116 @@ def config_to_db():
     db.update_all(sql, config_list)
     db.close()
 
+def daemonize(pid_file, rootdir):
+    pid = os.fork()
+    if pid:
+        sys.exit(0)
+    os.chdir(rootdir)
+    os.umask(0)
+    os.setsid()
+
+    _pid = os.fork()
+    if _pid:
+        sys.exit(0)
+
+    sys.stdout.flush()
+    sys.stderr.flush()
+
+    # dup2函数原子化地关闭和复制文件描述符，重定向到/dev/nul，即丢弃所有输入输出
+    with open('/dev/null') as read_null, open('/dev/null', 'w') as write_null:
+        os.dup2(read_null.fileno(), sys.stdin.fileno())
+        os.dup2(write_null.fileno(), sys.stdout.fileno())
+        os.dup2(write_null.fileno(), sys.stderr.fileno())
+
+    # 写入pid文件
+    if pid_file:
+        with open(pid_file, 'w+') as f:
+            pid=os.getpid()
+            logger.logger.info(f"Main Pid: {pid}")
+            f.write(str(pid))
+        atexit.register(os.remove, pid_file)
+
 def main():
-    #record()
-    check_item=[record, show, analysis]
+    init("./data/auto.db", "./share/init.sql")
+    config_to_db("./conf/autocheck.conf")
+    daemonize('logs/autocheck.pid', rootdir)
+    monkey.patch_all()
+
+    check_item=[record.record,  show.show,  analysis.analysis]
     gevent_list=[]
     for i in check_item:
         g=gevent.spawn(i, )
         gevent_list.append(g)
     gevent.joinall(gevent_list)
 
-    """
-    os.makedirs("report",  exist_ok=True)
-    printf(f"开始巡检时间: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
-    hostname=conf.get("autocheck", "hostname")[0]
-    if hostname is None:
-        hostname=socket.gethostname()
-    printf(f"巡检主机: {hostname}")
-    printf("*"*80)
-
-    host.info()
-    printf("-"*80)
-    tomcat.stats()
-    redis.stats()
-    mysql.stats()
-    backup.cat()
-    oracle.info()
-
-    printf("*"*80)
-    printf(f"结束巡检时间: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
-
-    # 巡检报告打包
-    report_files=os.listdir(".")
-    for report_file in report_files:
-        if report_file.startswith("report") and report_file.endswith("tar.gz"):
-            os.remove(report_file)
-    with tarfile.open(f"report-{datetime.datetime.now().strftime('%Y%m%d%H%M')}.tar.gz", "w:gz") as tar:
-        tar.add("./report")
-
-    mail.send()
-    timing.timing()
-    """
-
-def control(action):
+def control(action, pid=None):
     if action=="start":
-        pass
+        if pid is None:
+            logger.logger.info("程序启动...")
+            print(f"启动程序...")
+            main()
+        else:
+            print(f"程序({pid})正在运行...")
     elif action=="stop":
-        pass
+        if pid is None:
+            print(f"程序未运行...")
+        else:
+            print("程序关闭...")
+            logger.logger.info("程序关闭...")
+            os.kill(pid, 9)
+    elif action=="status":
+        if pid is None:
+            print("程序未运行...")
+        else:
+            print(f"程序({pid})正在运行...")
 
 def usage(action):
+    pid=get_pid("./logs/autocheck.pid")
     if action=="usage":
         print(f"Usage: {sys.argv[0]} start|stop|restart|status|sendmail")
     elif action=="start":
-        pid=get_pid()
-        if pid is None:
-            control("start")
-        else:
-            pass
+        control("start", pid)
     elif action=="restart":
-        control("stop")
-        control("start")
+        control("stop", pid)
+        time.sleep(1)
+        control("start", None)
     elif action=="stop":
-        control("stop")
+        control("stop", pid)
+    elif action=="status":
+        control("status", pid)
     elif action=="sendmail":
         pass
     else:
-        usage()
+        usage("usage")
 
-def get_pid():
-    pid_file="./logs/autocheck.pid"
+def get_pid(pid_file):
     if os.path.exists(pid_file):
         with open(pid_file, "r", encoding="utf8") as f:
-            pid=int(f.read())
-        if psutil.pid_exists(pid):
-            return pid
+            pid=f.read()
+            if pid != '':
+                pid=int(pid)
+                if psutil.pid_exists(pid):
+                    cmdline=",".join(psutil.Process(pid).cmdline())
+                    if "python" in cmdline and "main.py" in cmdline:
+                        return pid
     return None
-
-def init():
-    """数据初始化
-    """
-    os.makedirs("./data", exist_ok=True)
-    data_file='data/auto.db'
-    data_init_file='share/init.sql'
-    if os.path.exists(data_file):
-        pass
-    else:
-        logger.logger.info("开始初始化数据.")
-        db=database.db(data_file)
-        with open(data_init_file, "r") as f:
-            for sql in f.readlines():
-                db.update_one(sql)
-        logger.logger.info("初始化数据完成.")
 
 if __name__ == "__main__":
     if len(sys.argv)==2:
+        rootdir=os.path.dirname(__file__)
+        os.chdir(rootdir)
+        cfg=configparser.ConfigParser()
+        cfg.read("./conf/autocheck.conf")
+        os.makedirs("./logs", exist_ok=True)
+        log_file=get_config(cfg, "logs", "log_file")
+        log_level=get_config(cfg, "logs", "log_level")
+        try:
+            logger=log.Logger(log_file, log_level)
+        except Exception as e:
+            print(f"Error: {e}")
+            exit()
         action=sys.argv[1]
+        usage(action)
     else:
         usage("usage")
-        exit()
-
-    monkey.patch_all()
-    rootdir=os.path.dirname(__file__)
-    os.chdir(rootdir)
-
-    cfg=configparser.ConfigParser()
-    cfg.read("./conf/autocheck.conf")
-    os.makedirs("./logs", exist_ok=True)
-    log_file=get_config(cfg, "logs", "log_file")
-    log_level=get_config(cfg, "logs", "log_level")
-    try:
-        logger=log.Logger(log_file, log_level)
-    except Exception as e:
-        print(f"Error: {e}")
-        exit()
-
-    logger.logger.info("程序启动...")
-    init()
-    config_to_db()
-
-    #shutil.rmtree("./report/", ignore_errors=True)
-    main()
-
 
