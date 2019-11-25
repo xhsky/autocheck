482ccddde5d9e34d5876a03479f7b01c83f8ddd6
sky <x_hsky@163.com>
2019-11-25 18:32:06 +0800

    添加定时清理数据功能

---
+++ core/clean.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# *-* coding:utf8 *-*
+# sky
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+from lib import log, conf, database
+
+def clean_data(logger, keep_days):
+    logger.logger.info("开始清理数据...")
+    db=database.db()
+    all_tables=db.query_all("select name from sqlite_master where type='table'")
+    now_time=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    for i in all_tables:
+        sql=f"delete from {i} where record_time < datetime({now_time}, '-{keep_days} day')"
+        db.update_one(sql)
+    logger.logger.info("结束清理数据...")
+
+def clean():
+    log_file, log_level=log.get_log_args()
+    logger=log.Logger(log_file, log_level)
+    logger.logger.info("清理程序启动...")
+
+    keep_days=conf.get("autocheck", "keep_days")[0]
+
+    scheduler=BlockingScheduler()
+    scheduler.add_job(clean_data, 'cron', args=[logger, int(keep_days)], day_of_week='0-6', hour=1, minute=10, id=f'clean')
+    scheduler.start()
+    
+if __name__ == "__main__":
+    main()
