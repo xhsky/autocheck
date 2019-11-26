eb058e1818d3a6a6d73e690f91ced9000fd91f64
sky <x_hsky@163.com>
2019-11-26 13:59:50 +0800

    写入错误日志方式改为a

--- main.py
+++ main.py
@@ -76,7 +76,7 @@ def daemonize(pid_file, rootdir):
 
     # dup2函数原子化地关闭和复制文件描述符，重定向到/dev/nul，即丢弃所有输入输出
     #with open('/dev/null') as read_null, open('/dev/null', 'w') as write_null:
-    with open('/dev/null') as read_null, open('./logs/errors.log', 'w') as write_null:
+    with open('/dev/null') as read_null, open('./logs/errors.log', 'a') as write_null:
         os.dup2(read_null.fileno(), sys.stdin.fileno())
         os.dup2(write_null.fileno(), sys.stdout.fileno())
         os.dup2(write_null.fileno(), sys.stderr.fileno())
