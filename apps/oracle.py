2baaf15eceab16ace5c0d29b187f16a044362424
sky <x_hsky@163.com>
2019-11-27 14:31:07 +0800

    awr存储位置改为变量

--- apps/oracle.py
+++ apps/oracle.py
@@ -5,90 +5,13 @@
 import subprocess
 from lib import database, mail, log, warning
 import datetime
-#from lib import conf
-#from lib.tools import format_size
-#from lib.printf import printf
 import shutil
 
-'''
-def info():
-    check, awr_hours=conf.get("oracle", 
-            "check", 
-            "awr_hours"
-            )
-    
-    if check=="1":
-        printf("Oracle信息:")
-        # 表空间
-        printf("表空间信息:")
-        sql="""
-        set heading off trimspool on feedback off pagesize 0 linesize 1000
-        SELECT a.tablespace_name ,
-          a.bytes,
-          ( a.bytes - b.bytes ),
-          b.bytes,
-          Round(( ( a.bytes - b.bytes ) / a.bytes ) * 100, 2)
-        FROM  (SELECT tablespace_name,
-                      SUM(bytes) bytes
-                FROM  dba_data_files
-                GROUP  BY tablespace_name) a,
-              (SELECT tablespace_name,
-                      SUM(bytes) bytes,
-                      Max(bytes) largest
-                FROM  dba_free_space
-                GROUP  BY tablespace_name) b
-        WHERE  a.tablespace_name = b.tablespace_name
-        ORDER  BY ( ( a.bytes - b.bytes ) / a.bytes ) DESC;
-        """
-        cmd=f"su - oracle -c 'sqlplus -S / as sysdba <<EOF\n{sql}\nEOF'"
-        (status, message)=subprocess.getstatusoutput(cmd)
-        if status==0:
-            flag=0
-            printf(f"表空间名称    表空间大小    已使用表空间    未使用表空间    使用率(%)")
-            for i in message.splitlines():
-                i=i.split()
-                row=f"{i[0]:<14}{format_size(i[1]):<14}{format_size(i[2]):<17}{format_size(i[3]):<17}{i[4]:<4}"
-                printf(row)
-                if float(i[4]) > 95:
-                    printf(f"Oracle: {i[0]}表空间不足, 已使用{i[4]}%", 1)
-                    flag=1
-            if flag==0:
-                printf(f"Oracle: 表空间正常", 1)
-
-            # awr
-            printf("-"*40)
-            printf("awr信息:")
-            awr_format="html"
-            awr_file='/tmp/awr.html'
-            days=4
-            sql="""
-            set heading off trimspool on feedback off pagesize 0
-            select trim(max(snap_id)) from dba_hist_snapshot;
-            """
-            cmd=f"su - oracle -c 'sqlplus -S / as sysdba <<EOF\n{sql}\nEOF'"
-            (status, message)=subprocess.getstatusoutput(cmd)
-            max_snap_id=int(message)
-            if int(awr_hours) > max_snap_id:
-                awr_hours=max_snap_id
-            min_snap_id=max_snap_id-int(awr_hours)
-            cmd=f"""su - oracle -c"
-            echo -e '{awr_format}\n{days}\n{min_snap_id}\n{max_snap_id}\n{awr_file}\n' | (sqlplus -S / as sysdba @?/rdbms/admin/awrrpt.sql)"
-            """
-            (status, message)=subprocess.getstatusoutput(cmd)
-            if status==0:
-                shutil.move(awr_file, "./report/")
-                printf("请查看awr.html文件")
-            else:
-                printf("awr生成失败, 请手动生成")
-        else:
-            printf("无法连接Oracle.")
-'''
-def generate_awr(awr_hours):
+def generate_awr(awr_hours, report_dir):
     """生成awr
     """
     awr_format="html"
     awr_file='/tmp/awr.html'
-    #awr_file="./report/"
     days=4
     sql="""
     set heading off trimspool on feedback off pagesize 0
@@ -104,7 +27,7 @@ def generate_awr(awr_hours):
     echo -e '{awr_format}\n{days}\n{min_snap_id}\n{max_snap_id}\n{awr_file}\n' | (sqlplus -S / as sysdba @?/rdbms/admin/awrrpt.sql)"
     """
     (status, message)=subprocess.getstatusoutput(cmd)
-    shutil.move(awr_file, "./report/awr.html")
+    shutil.move(awr_file, f"{report_dir}/awr.html")
     return status
 
 def record(log_file, log_level):
